2ef2fd7ec5750b453fde7d817135dc6182a18e4b
Auzel <filey6@hotmail.com>
2020-04-12 21:59:14 +0000

    added models and authentication

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import json
 from flask_cors import CORS
 from flask import Flask, request, render_template
 from sqlalchemy.exc import IntegrityError
+from flask_jwt import JWT, jwt_required, current_identity
+from datetime import timedelta 
 
-from models import db, Logs
+from models import db, MedInstitution, User, Physician, Patient, MedHistory
 
 ''' Begin boilerplate code '''
 def create_app():
   app = Flask(__name__, static_url_path='')
   app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b4ab06921840a3:995a5935@us-cdbr-iron-east-01.cleardb.net/heroku_b2abbb44d079db0'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
- ##app.config['SECRET_KEY'] = "MYSECRET"    
+  ##app.config['SECRET_KEY'] = "MYSECRET"    
+  app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7)   
   CORS(app)
   db.init_app(app)
   return app
@@ -20,6 +23,19 @@ app = create_app()
 app.app_context().push()
 
 ''' End Boilerplate Code '''
+
+''' Set up JWT here '''
+def authenticate(uname, password):
+  user=User.query.filter_by(username=uname).first()
+  if user and user.check_password(password):
+    return user
+
+def identity(payload):
+  return User.query.get(payload['identity'])
+  
+jwt = JWT(app,authenticate,identity)
+''' End JWT Setup '''
+
 
 @app.route('/')
 def index():
--- models.py
+++ models.py
@@ -1,18 +1,84 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
 
-class Logs(db.Model):
+class MedInstitution:
     id = db.Column(db.Integer, primary_key=True)
-    studentId =  db.Column(db.Integer, nullable=False)
-    stream = db.Column(db.Integer, nullable=False)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    address = db.Column(db.String(50), nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fName = db.Column(db.String(20), nullable=False)
+    lName = db.Column(db.String(20), nullable=False)
+    username=db.Column(db.String(20), nullable=False)
+    password=db.Column(db.String(20), nullable=False)
+    dateOfBirth=db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    address=db.Column(db.String(50), nullable=False)
 
     def toDict(self):
         return{
             'id': self.id,
-            'studentId': self.studentId,
-            'stream': self.stream,
-            'created': self.created.strftime("%m/%d/%Y, %H:%M:%S")
+            'fName': self.fName,
+            'lName': self.lName,
+            'username': self.username,
+            'password': self.password,            
+            'dateOfBirth': self.created.strftime("%m/%d/%Y, %H:%M:%S"),
+            'address': self.address
         }
+
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    ##finish authentication
+
+
+class Physician(User):
+    type1= db.Column(db.String(20), nullable=False)
+    degree=db.Column(db.String(20), nullable=False)
+    placeOfEducation=db.Column(db.String(20), nullable=False)
+    ##appointment date scheduling
+
+    def toDict(self):
+        return{
+            self().toDict().update({
+                'type1':self.type1,
+                'degree':self.degree,
+                'placeOfEducation':self.placeOfEducation
+            })   
+        }
+
+class Patient(User):
+    currentProblem = db.Column(db.String(20), nullable=False)
+    currentTreatment=db.Column(db.String(20), nullable=False)
+
+    def toDict(self):
+        return{
+            self().toDict().update({
+                'currentProblem':self.currentProblem,
+                'currentTreatment':self.currentTreatment,
+                'placeOfEducation':self.placeOfEducation
+            })   
+        }
+
+class MedHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)    
+    description=db.Column(db.String(1000), nullable=False)
+    ##create new table for separate allergies since 
+
+    def toDict(self):
+        return{
+            'id':self.id,
+            'description':self.description   
+        }
+    
