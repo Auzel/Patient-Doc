bd93d1836d86d75e2a38c4c4dcc108b84d8b764a
PhilUWI3 <alphonsus.roberts@hotmail.com>
2020-05-20 09:43:50 +0000

    change MYSQL to Postgresql to stop heroku from going down

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(engine_options={"pool_recycle": 60})
+db = SQLAlchemy()
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
